ced5c6dd386a383da27df56bce76f320d135df35
AprenentEMB <aprenentaprogramar@gmail.com>
2026-03-30 12:33:04 +0200

    fix 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://graduacions.cat',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
