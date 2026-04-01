70de3a710d242b7f44b8dea4483c42a7f0c296ac
AprenentEMB <aprenentaprogramar@gmail.com>
2026-04-01 12:21:05 +0200

    adding correct url on astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://graduacions.cat',
+  site: 'https://www.graduacions.cat',
   adapter: vercel(),
   integrations: [sitemap()],
   vite: {
